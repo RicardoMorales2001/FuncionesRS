5ccc2d773630c74a9ee4bcb7a06ff7cc13db38c6
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2023-03-28 22:45:32 -0500

    Descripcion y creacion de los test

--- tests/testthat/test-regresionlineal.R
+++ tests/testthat/test-regresionlineal.R
@@ -1,18 +1,10 @@
 library(testthat)
 
-x <- c(1, 2, 3, 4, 5)
-y <- c(2, 4, 6, 8, 10)
-
-test_that("La función devuelve un objeto modelo", {
-  expect_s3_class(reg_lineal(x, y), "lm")
-})
-
-test_that("La función traza un gráfico", {
-  plot_output <- capture.output(reg_lineal(x, y))
-  expect_match(plot_output, "Regresión lineal")
-})
-
 test_that("La función genera una excepción cuando los vectores tienen diferentes longitudes", {
-  expect_error(reg_lineal(x, c(y, 12)))
+  expect_error(reg_lineal(x, c(y, 12)), "Los vectores deben tener el mismo tamaño.")
 })
 
+test_that("La función genera una excepción cuando x o y son nulos", {
+  expect_error(reg_lineal(NULL, y), "x is NULL")
+  expect_error(reg_lineal(x, NULL), "y is NULL")
+})
